b3f556ca6ded07d7c4bbca6d21d9bb77015fb4d4
lutfiqaraman <lutfiqaraman@gmail.com>
2023-05-07 18:41:56 +0300

    building the home page

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: 'Next Project',
+    description: 'Building a project using Next framework'
+};
+
+const RootLayout = ({children}) => {
+    return (
+        <html lang="en">
+            <body>
+                <div className="main">
+                    <div className="gradient"/>
+                </div>
+                <main className="app">
+                    {children}
+                </main>
+            </body>
+        </html>
+    );
+};
+
+export default RootLayout;
