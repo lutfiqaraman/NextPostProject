4a2e6127bd5bb497da25e1b294998ca064f86d90
lutfiqaraman <lutfiqaraman@gmail.com>
2023-05-14 19:56:24 +0300

    build the home page

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,8 @@ const Home = () => {
     return (
         <section className="w-full flex-center flex-col">
             <h1 className="head_text text-center">Discover & Share</h1>
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center">AI-Powered Prompts</span>
         </section>
     );
 };
