009bcdba44f2eeb0512ab9343a45208ea418ac3d
lutfiqaraman <lutfiqaraman@gmail.com>
2023-05-17 21:41:43 +0300

    build the home page

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ const Home = () => {
             <h1 className="head_text text-center">Discover & Share</h1>
             <br className="max-md:hidden" />
             <span className="orange_gradient text-center">AI-Powered Prompts</span>
+            <p className="desc text-center">Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts</p>
         </section>
     );
 };
