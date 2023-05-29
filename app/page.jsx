47dba070f04bdfe4e069f132f152d19703f682e3
lutfiqaraman <lutfiqaraman@gmail.com>
2023-05-29 11:07:34 +0300

    use the component

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from 'components/Feed'
+
 const Home = () => {
     return (
         <section className="w-full flex-center flex-col">
@@ -5,6 +7,8 @@ const Home = () => {
             <br className="max-md:hidden" />
             <span className="orange_gradient text-center">AI-Powered Prompts</span>
             <p className="desc text-center">Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts</p>
+
+            <Feed/>
         </section>
     );
 };
